71ce55b22852f93bdae80249587c303733a17a11
Andrew Wladis <andrewwladis@gmail.com>
2023-12-30 16:57:47 -0500

    Made it so the skill icons glow where the mouse is

--- src/app/Skills.tsx
+++ src/app/Skills.tsx
@@ -1,54 +1,128 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaHtml5, FaCss3Alt, FaJs, FaNodeJs, FaReact, FaGitAlt, FaPython } from "react-icons/fa";
 import { BiLogoTypescript } from "react-icons/bi";
 import { IoLogoFirebase } from "react-icons/io5";
 import { SiAdobeillustrator, SiAdobephotoshop, SiAdobexd, SiMongodb } from "react-icons/si";
 
-export default function Skills() {
-  return (
-    <section id="skills" style={{ margin: 10, marginTop: 100 }}>
-        <h3 className='text-xl font-semibold text-center text-white'>I am proficient in...</h3>
-        <div className='w-full flex flex-row flex-wrap items-center justify-center' style={{ margin: 10 }}>
-          <div style={{ margin: 5 }}>
-            <FaHtml5 size="4em" />
-          </div>
-          <div style={{ margin: 5 }}>
-            <FaCss3Alt size="4em" />
-          </div>
-          <div style={{ margin: 5 }}>
-            <FaJs size="4em" />
-          </div>
-          <div style={{ margin: 5 }}>
-            <BiLogoTypescript size="4.75em" />
-          </div>
-          <div style={{ margin: 5 }}>
-            <FaNodeJs size="4em" />
-          </div>
-          <div style={{ margin: 5 }}>
-            <FaReact size="4em" />
-          </div>
-          <div style={{ margin: 5 }}>
-            <FaGitAlt size="4em" />
-          </div>
-          <div style={{ margin: 5 }}>
-            <SiMongodb size="4em" />
-          </div>
-          <div style={{ margin: 5 }}>
-            <FaPython size="4em" />
-          </div>
-          <div style={{ margin: 5 }}>
-            <IoLogoFirebase size="3.75em" />
-          </div>
-          <div style={{ margin: 5 }}>
-            <SiAdobephotoshop size="3.5em" />
-          </div>
-          <div style={{ margin: 5 }}>
-            <SiAdobexd size="3.5em" />
-          </div>
-          <div style={{ margin: 5 }}>
-            <SiAdobeillustrator size="3.5em" />
-          </div>
-        </div>
-      </section>
-  )
+interface SkillsProps {
+    cursorX: number;
 }
+  
+const Skills: React.FC<SkillsProps> = ({ cursorX }) => {
+    const [windowWidth, setWindowWidth] = useState(0);
+
+    useEffect(() => {
+        // Function to update window width
+        const updateWindowWidth = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        // Initial window width
+        updateWindowWidth();
+
+        // Add event listener for window resize
+        window.addEventListener('resize', updateWindowWidth);
+
+        // Cleanup: remove event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', updateWindowWidth);
+        };
+    }, []);
+
+    useEffect(() => {
+        console.log(windowWidth)
+    }, [windowWidth])
+
+    return (
+        <section id="skills" style={{ margin: 10, marginTop: 100, marginBottom: 100 }}>
+            <h3 className='text-xl font-semibold text-center text-white'>My Technical Skills Include:</h3>
+            {(windowWidth > 1000) ? (
+                <div className='w-full flex flex-row flex-wrap items-center justify-center' style={{ margin: 10 }}>
+                    <div style={{ margin: 5 }}>
+                        <FaHtml5 size="4em" style={{ opacity: (((((windowWidth) / 15) * 2) - cursorX) / 1250) + 0.7 }}/>
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <FaCss3Alt size="4em" style={{ opacity: (((((windowWidth) / 15) * 3) - cursorX) / 1250) + 0.7 }}/>
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <FaJs size="4em" style={{ opacity: ((((windowWidth / 15) * 4) - cursorX) / 1250) + 0.7 }}/>
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <BiLogoTypescript size="4.75em" style={{ opacity: ((((windowWidth / 15) * 5) - cursorX) / 1250) + 0.7 }}/>
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <FaNodeJs size="4em" style={{ opacity: ((((windowWidth / 15) * 6) - cursorX) / 1250) + 0.7 }}/>
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <FaReact size="4em" style={{ opacity: ((((windowWidth / 15) * 7) - cursorX) / 1250) + 0.7 }}/>
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <FaGitAlt size="4em" style={{ opacity: ((((windowWidth / 15) * 8) - cursorX) / 1250) + 0.7 }}/>
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <SiMongodb size="4em" style={{ opacity: ((((windowWidth / 15) * 7) - (windowWidth - cursorX)) / 1250) + 0.7 }}/>
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <FaPython size="4em" style={{ opacity: ((((windowWidth / 15) * 6) - (windowWidth - cursorX)) / 1250) + 0.7 }} />
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <IoLogoFirebase size="3.75em" style={{ opacity: ((((windowWidth / 15) * 5) - (windowWidth - cursorX)) / 1250) + 0.7 }} />
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <SiAdobephotoshop size="3.5em" style={{ opacity: ((((windowWidth / 15) * 4) - (windowWidth - cursorX)) / 1250) + 0.7 }} />
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <SiAdobexd size="3.5em" style={{ opacity: ((((windowWidth / 15) * 3) - (windowWidth - cursorX)) / 1250) + 0.7 }} />
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <SiAdobeillustrator size="3.5em" style={{ opacity: ((((windowWidth / 15) * 2) - (windowWidth - cursorX)) / 1250) + 0.7 }} />
+                    </div>
+                </div>
+            ) : (
+                <div className='w-full flex flex-row flex-wrap items-center justify-center' style={{ margin: 10 }}>
+                    <div style={{ margin: 5 }}>
+                        <FaHtml5 size="4em" />
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <FaCss3Alt size="4em" />
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <FaJs size="4em" />
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <BiLogoTypescript size="4.75em" />
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <FaNodeJs size="4em" />
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <FaReact size="4em" />
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <FaGitAlt size="4em" />
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <SiMongodb size="4em" />
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <FaPython size="4em" />
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <IoLogoFirebase size="3.75em" />
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <SiAdobephotoshop size="3.5em" />
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <SiAdobexd size="3.5em" />
+                    </div>
+                    <div style={{ margin: 5 }}>
+                        <SiAdobeillustrator size="3.5em" />
+                    </div>
+                </div>
+            )}
+        </section>
+    )
+}
+
+export default Skills
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Services from './Services';
 import Skills from './Skills';
 
 export default function Home() {
+  const [cursorX, setCursorX] = useState(0);
   const [windowWidth, setWindowWidth] = useState(0);
 
   useEffect(() => {
@@ -25,16 +26,23 @@ export default function Home() {
     };
   }, []);
 
+  const handleMouseMove = (e: any) => {
+      setCursorX(e.clientX);
+  };
+
   return (
-    <main style={{ background: "linear-gradient(164deg, rgba(18,17,17,1) 0%, rgba(45,23,46,1) 35%, rgba(25,25,25,1) 100%)" }} className="w-full">
+    <main onMouseMove={handleMouseMove} style={{ background: "linear-gradient(164deg, rgba(18,17,17,1) 0%, rgba(45,23,46,1) 35%, rgba(25,25,25,1) 100%)" }} className="w-full">
       <Navigation />
-      <section id="about" className='w-full flex justify-center' style={{ marginTop: 250 }}>
+      <section id="about" className='w-full flex justify-center' style={{ paddingTop: 250 }}>
         <p className='text-xl font-semibold text-center text-white' style={{ marginRight: (windowWidth / 6), marginLeft: (windowWidth / 6) }}>
           Welcome, I'm <b>Andrew Wladis</b>, a passionate and creative freelance software developer dedicated to crafting visually appealing websites that not only captivate viewers but also elevate brands.
         </p>
       </section>
       <Services />
-      <Skills />
+      <Skills cursorX={cursorX} />
+      <section id="contact" className='w-full flex justify-center'>
+        <h3 className='text-xl font-semibold text-center text-white'>Feel free to reach out to me using the contact information below:</h3>
+      </section>
     </main>
   )
 }
