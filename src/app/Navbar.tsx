3af5a42c46aab1ce9168f1150570f55918fe0a25
Andrew Wladis <andrewwladis@gmail.com>
2023-12-29 20:13:23 -0500

    Initial commit

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,83 @@
+"use client";
+import { Navbar, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, NavbarContent, NavbarItem, Link, Button, NavbarBrand } from "@nextui-org/react";
+import React, { useEffect, useState } from 'react';
+
+export default function Navigation() {
+    const [isMenuOpen, setIsMenuOpen] = useState(true);
+    const [isDesktop, setIsDesktop] = useState(false);
+
+    useEffect(() => {
+      if (typeof window !== 'undefined') {
+        setIsDesktop(window.innerWidth < 600);
+
+        const handleResize = () => {
+          setIsDesktop(window.innerWidth < 600);
+        };
+  
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }
+    }, []);
+
+    const menuItems = [
+        "About",
+        "Services",
+        "Portfolio",
+        "Skills",
+        "Contact"
+    ];
+
+    return (
+        <>
+            {isDesktop ? (
+                <Navbar onMenuOpenChange={setIsMenuOpen} className='bg-white'>
+                    <NavbarContent>
+                        <NavbarMenuToggle
+                            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                            className="sm:hidden"
+                        />
+                        <NavbarBrand>
+                            <h1 className="text-xl font-semibold">Andrew Wladis</h1>
+                        </NavbarBrand>
+                    </NavbarContent>
+                    <NavbarMenu className='bg-white shadow-lg'>
+                        {menuItems.map((item, index) => (
+                            <NavbarMenuItem key={`${item}-${index}`}>
+                                <Link
+                                    className="w-full text-black"
+                                    href={`#${item.toLowerCase()}`}
+                                    size="lg"
+                                >
+                                    {item}
+                                </Link>
+                            </NavbarMenuItem>
+                        ))}
+                    </NavbarMenu>
+                </Navbar>
+            ) : (
+                <Navbar onMenuOpenChange={setIsMenuOpen} className='bg-white'>
+                    <NavbarContent>
+                        <NavbarBrand>
+                            <h1 className="text-xl font-semibold">Andrew Wladis</h1>
+                        </NavbarBrand>
+                        {menuItems.map((item, index) => (
+                            <NavbarMenuItem key={`${item}-${index}`}>
+                                <Link
+                                    className="w-full text-black"
+                                    href={`#${item.toLowerCase()}`}
+                                    size="lg"
+                                >
+                                    {item}
+                                </Link>
+                            </NavbarMenuItem>
+                        ))}
+                    </NavbarContent>
+                </Navbar>
+            )}
+        </>
+
+    )
+}
